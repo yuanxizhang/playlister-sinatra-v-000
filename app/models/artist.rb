5d245ecc3cadb818997f0d5baf42baf8cdbc3ec3
Yancy Zhang <yuanxi.zhang@gmail.com>
2019-02-09 00:53:41 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -4,6 +4,7 @@ class Artist < ActiveRecord::Base
   
   # extend Slugifiable::ClassMethods
   # include Slugifiable::InstanceMethods
+<<<<<<< HEAD
   
   def slug
       self.name.strip.gsub(" ", "-").downcase
@@ -12,4 +13,6 @@ class Artist < ActiveRecord::Base
   def self.find_by_slug(slug)
       self.all.find {|item| item.slug == slug}
   end
+=======
+>>>>>>> 7e86cad77b0abd8baf38c972f189607dda439af1
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -5,6 +5,7 @@ class Song < ActiveRecord::Base
 
   # extend Slugifiable::ClassMethods
   # include Slugifiable::InstanceMethods
+<<<<<<< HEAD
 
   def slug
       self.name.strip.gsub(" ", "-").downcase
@@ -13,4 +14,6 @@ class Song < ActiveRecord::Base
   def self.find_by_slug(slug)
       self.all.find {|item| item.slug == slug}
   end
+=======
+>>>>>>> 7e86cad77b0abd8baf38c972f189607dda439af1
 end
